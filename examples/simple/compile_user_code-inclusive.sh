5190c9889531d4983acc8a95328702d25464aac8
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2017-03-18 14:33:21 +0100

    DOC: committign the rest of this dead try.

--- examples/simple/compile_user_code-inclusive.sh
+++ examples/simple/compile_user_code-inclusive.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "g++ -Iinclude/ example_user_code.cpp -o example_user_code"
-g++ -Iinclude/ example_user_code.cpp -o example_user_code
+g++ -Iinclude/ example_user_code.cpp -o example_user_code -v
--- examples/simple/include/somelib/somelib.hpp
+++ examples/simple/include/somelib/somelib.hpp
@@ -5,10 +5,12 @@
 
 #include <iostream>
 #include INCLUSIVE(boost_preprocessor,boost/preprocessor/stringize.hpp)
+#include INCLUSIVE(boost_preprocessor,boost/preprocessor/repeat.hpp)
+#include INCLUSIVE(boost_preprocessor,boost/preprocessor/cat.hpp)
 
 namespace somelib {
   inline void hello_world() {
-    std::cout << BOOST_PP_STRINGIZE(HELLO World!!!) << std::endl;
+    std::cout << BOOST_PP_STRINGIZE(HELLO World!!! BOOST_PP_CAT(cool,_fool)) << std::endl;
   }
 }
 
