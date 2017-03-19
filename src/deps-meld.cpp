c1fea50310925e505dd941526d6f64d6cebc607a
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2017-03-19 11:15:02 +0100

    FEATURE: Support for transitive libraries via glue.transitive.

--- src/deps-meld.cpp
+++ src/deps-meld.cpp
@@ -17,6 +17,7 @@ namespace inclusive {
   namespace bp = boost::process;
   namespace fs = boost::filesystem;
 
+  constexpr auto GLUE_PATH = "deps/glue";
 
   struct dep {
     //! The github path or a git clone uri.
@@ -27,6 +28,9 @@ namespace inclusive {
 
     //! The subrepository path to add to include path.
     std::string include_path;
+
+    //! True when dependency is needed because of another dependency project.
+    bool transitive{};
 
     //! \return The gihub clone URI if simple-single-slash path otherwise git_uri as-is.
     std::string get_uri() const {
@@ -53,15 +57,11 @@ namespace inclusive {
 
   };
 
-  //! Reads the list of deps in the deps.inclusive json file.
+  //! Reads the list of deps in the from the given  file, and if it exits it's accompanying .transitive.
   inline std::map<std::string, dep> read_deps(const std::string& path) {
-    std::ifstream ifs(path.data(), std::ios::in );
-
-    nlohmann::json j;
-    ifs >> j;
-
     std::map<std::string, dep> deps;
-    for (nlohmann::json::iterator it = j.begin(); it != j.end(); ++it) {
+    
+    auto to_dep = [](nlohmann::json::iterator& it) {
       dep d;
       d.git_uri = it.key();
       d.ref = it.value()["@"].get<std::string>();
@@ -70,36 +70,115 @@ namespace inclusive {
         d.include_path = it.value()["?"].get<std::string>();
       }
 
-      deps[d.git_uri] = d;
+      return d;
+    };   
+
+    { // Direct dependencies
+      std::ifstream ifs(path.data(), std::ios::in );
+
+      nlohmann::json j;
+      ifs >> j;
+      for (nlohmann::json::iterator it = j.begin(); it != j.end(); ++it) {
+        auto d = to_dep(it);
+        deps[d.git_uri] = d;
+      }
+    }
+
+    auto transitive_path = path + ".transitive";
+    if (fs::exists(transitive_path)) { // Transitive dependencies
+      std::ifstream ifs(transitive_path.data(), std::ios::in );
+
+      nlohmann::json j;
+      ifs >> j;
+      for (nlohmann::json::iterator it = j.begin(); it != j.end(); ++it) {
+        auto d = to_dep(it);
+        d.transitive = true;
+        deps[d.git_uri] = d;
+      }
     }
 
     return deps;
+  }
+
+  //! Reads the list of deps in the deps.inclusive json file.
+  inline void write_deps(const std::string& path, const std::map<std::string, dep>& deps, bool write_direct = false) {
+    auto to_json = [](auto d) {
+      nlohmann::json dep_j;
+      dep_j["@"] = d.second.ref;
+
+      if (d.second.include_path != "") {
+        dep_j["-I"] = d.second.include_path;
+      }
+
+      return dep_j;
+    };
+
+    if (write_direct) { // Direct dependencies
+      std::fstream ofs(path.data(), std::ios::in | std::ios::out | std::ios::trunc );
+      nlohmann::json j;
+
+      for (auto& dep_entry : deps ) {
+        if (!dep_entry.second.transitive) {
+          j[dep_entry.first] = to_json(dep_entry);
+        }
+      }
+
+      ofs << j.dump(2);
+    }
+
+    { // Transitive dependencies
+
+      nlohmann::json j;
+      for (auto& dep_entry : deps ) {
+        if (dep_entry.second.transitive) {
+          j[dep_entry.first] = to_json(dep_entry);
+        }
+      }
+
+      if (j.size() > 0) {
+        auto transitive_path = (path + ".transitive");
+        std::fstream ofs(transitive_path.data(), std::ios::in | std::ios::out | std::ios::trunc );
+        ofs << j.dump(2);
+      }
+    }
   }
 
 
   //! Run git to install the dep if not already present
   inline void check_and_clone(const dep& d) {
 
-    bp::system(bp::shell, "git", "subtree", (fs::exists(d.get_name()) ? "pull" : "add"),
+    auto returncode = bp::system(bp::shell, "git", "subtree", 
+      (fs::exists(fs::path("deps") / d.get_name()) ? "pull" : "add"),
       "--prefix", std::string("deps/") + d.get_name(),
       d.get_uri(), d.ref,
       "--squash");
 
+    if (returncode != 0) {
+      throw std::runtime_error("Cannot add the dependency, your working tree must be all commited first!");
+    }
   }
 
   //! Delves in each dependencies at 1 depth level and lookup for further includesive deps. If some pull them up here.
   inline void hikeup_deep_deps(std::map<std::string, dep>& deps) {
 
     for (auto& d : deps) {
-      auto tape = fs::path(d.second.get_name()) / "deps" / ".tape";
+      auto tape = fs::path("deps") / d.second.get_name() / GLUE_PATH;
       if (fs::exists(tape)) {
         auto deep_deps = read_deps(tape.native());
+
+        // For us they are all transitive
+        std::for_each(deep_deps.begin(), deep_deps.end(),
+          [](auto& p) { p.second.transitive  = true;});
+
         deps.insert(deep_deps.begin(), deep_deps.end());
       }
     }
 
+    // Write down the transitive deps
+    write_deps(GLUE_PATH, deps);   
+
     // Now that we have hiked up the deps of our deps, clone and add them again at our level.
-    // We have to duplicate the files because macOS, linux ok but windows-git-clone symlinks impossible.
+    // We have to duplicate the files because windows-git-clone symlinks impossible. Sad.
     for (auto& d : deps) { check_and_clone(d.second); }
   }
 
@@ -112,7 +191,7 @@ namespace inclusive {
   
 
 int main() {
-  auto deps = inclusive::read_deps("deps/.tape"); 
+  auto deps = inclusive::read_deps(inclusive::GLUE_PATH); 
 
   for (auto& d : deps) { inclusive::check_and_clone(d.second); }
   hikeup_deep_deps(deps);
